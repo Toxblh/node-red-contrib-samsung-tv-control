301114e83cdefc5b5e9b72989087f5d3f13dc83c
Anton Palgunov <toxblh@gmail.com>
2021-07-21 21:57:45 +0100

    Fix html for settings

--- __tests__/samsung_spec.js
+++ __tests__/samsung_spec.js
@@ -8,17 +8,17 @@ describe('Samsung-tv Node', function () {
 
   it.each`
     type
-      ${'samsung-tv-send'}
-      ${'samsung-tv-sendchannel'}
-      ${'samsung-tv-isalive'}
-      ${'samsung-tv-isaliveping'}
-      ${'samsung-tv-token'}
-      ${'samsung-tv-turnon'}
-      ${'samsung-tv-openapp'}
-      ${'samsung-tv-getapps'}
-  `('should be loaded $type', function({ type }, done) {
+    ${'samsung-tv-send'}
+    ${'samsung-tv-sendchannel'}
+    ${'samsung-tv-isalive'}
+    ${'samsung-tv-isaliveping'}
+    ${'samsung-tv-token'}
+    ${'samsung-tv-turnon'}
+    ${'samsung-tv-openapp'}
+    ${'samsung-tv-getapps'}
+  `('should be loaded $type', function ({ type }, done) {
     var flow = [{ id: 'n1', type, name: 'test name' }]
-    helper.load(samsung, flow, function() {
+    helper.load(samsung, flow, function () {
       var n1 = helper.getNode('n1')
       expect(n1).toHaveProperty('name', 'test name')
       done()
